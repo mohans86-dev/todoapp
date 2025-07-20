d92f5ba9ef28ea664f1bffdfbc8cba0c302da4d5
jaishriram86 <mohans86700@gmail.com>
2025-07-21 03:13:57 +0530

    added test api

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
+const expServer = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
 
-const expServer=require('express');
-const colors=require('colors');
-const morgan=require('morgan');
-const dotenv=require('dotenv');
-const connectDB=require('./db');
+const app = expServer();
 
-const app=expServer();
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(expServer.json({}));
-app.use(expServer.json({
-    extended:true
-}));
+app.use(
+  expServer.json({
+    extended: true,
+  })
+);
 
 dotenv.config({
-    path:'./config/config.env'
+  path: "./config/config.env",
 });
 
+//connect mongoDB
 connectDB();
 
-app.use('/api/todo/auth', require('./routes/user'));
-app.use('/api/todo', require('./routes/Todo'));
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    msg: "Successfully running",
+  });
+});
+app.use("/api/todo/auth", require("./routes/user"));
+app.use("/api/todo", require("./routes/Todo"));
 
-const PORT=process.env.PORT || 3000;
-app.listen(PORT, 
-    console.log(`Server is running on port: ${PORT}`.red.underline.bold));
+const PORT = process.env.PORT || 3000;
+app.listen(
+  PORT,
+  console.log(`Server is running on port: ${PORT}`.red.underline.bold)
+);
